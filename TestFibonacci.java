f451dbad45a52840847b9b8b61c1f62992cec029
yaqin-hong <146725990+yaqin-hong@users.noreply.github.com>
2023-10-02 19:20:13 +0800

    用多种方式实现要求功能

---
+++ TestFibonacci.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class TestFibonacci {
+
+    static Scanner input = new Scanner(System.in);
+
+    // here is the function you need to implement
+	public static void parse_line(int n, int d) {
+        // int a[]; 
+        int[] a = new int[n];
+        a[0]=1;
+        a[1]=1;
+        for (int i=2;i<n;i++){
+            a[i]=a[i-1]+a[i-2];
+        }
+        /*
+        for (int i=(n-1);i>=(n-d);i--){
+            System.out.print(a[i] + ", ");
+        }
+         */
+        /*
+        for (int i=0; i<d; i++) {
+            System.out.print(a[n-i-1] + ", ");
+        }
+         */
+        /*for (int i=1; i<=d; i++) {
+            System.out.print(a[n-i] + ", ");
+        }*/
+        int i = 0;
+        do{
+            System.out.print(a[n-i-1] + ", ");
+            i++;
+        } while (i<d);
+        System.out.println();
+
+        i = 0;
+        while (i<d) {
+            System.out.print(a[n-i-1] + ", ");
+            i++;
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int line_number = Integer.parseInt(input.nextLine()); 
+        for(int i=0; i<line_number; i++) {
+            String s = input.nextLine();
+            String t[] = s.split(", ");
+            int n = Integer.parseInt(t[0]);
+            int d = Integer.parseInt(t[1]);
+            TestFibonacci.parse_line(n, d);
+        }
+    }
+}
